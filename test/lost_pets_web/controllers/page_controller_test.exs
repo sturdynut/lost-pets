518875886cbda89d20969484c4a54be6885c9e1f
Matti Salokangas <matti.salokangas@gmail.com>
2019-12-20 15:53:28 -0800

    ✅ Update tests for home page

--- test/lost_pets_web/controllers/page_controller_test.exs
+++ test/lost_pets_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule LostPetsWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Lost Pets"
   end
 end
