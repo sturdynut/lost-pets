924aaf37432c4cacd25e1aab1713e8b078e01040
Matti Salokangas <matti.salokangas@gmail.com>
2019-12-20 15:18:48 -0800

    ✅ Add mix test watch for running tests in watch mode

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule LostPets.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false}
+      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false}
     ]
   end
 
